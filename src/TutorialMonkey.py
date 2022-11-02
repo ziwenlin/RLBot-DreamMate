a1f31b8c9b99f6b2c34a137d5f607933a3214398
ziwenlin <zw.lin1998@hotmail.com>
2022-11-02 18:30:27 +0100

    Created tutorial monkey
    This monkey does nothing other than
    trying stuff out purposes.

---
+++ src/TutorialMonkey.py
@@ -0,0 +1,13 @@
+from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
+
+
+class TutorialMonkey(BaseAgent):
+    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+
+
+        self.renderer.begin_rendering()
+        self.renderer.draw_string_3d((0, 0, 0), 2, 2, "Hello RLBot!", self.renderer.black())
+        self.renderer.end_rendering()
+
+        return SimpleControllerState()
