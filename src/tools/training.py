29bc75c61347067b3ebaf7340b1197f0531459ca
ziwenlin <zw.lin1998@hotmail.com>
2022-11-06 10:46:54 +0100

    Added flying upwards training

--- src/tools/training.py
+++ src/tools/training.py
@@ -31,6 +31,31 @@ def aerial_mid_field(index):
         game_info=game_info
     )
 
+def aerial_straight_up(index):
+    car_state = CarState(
+        boost_amount=100, physics=Physics(
+            location=Vector3(0, -100, 20),
+            rotation=Rotator(0, math.pi / 2, 0),
+            velocity=Vector3(0, 0, 0),
+            angular_velocity=Vector3(0, 0, 0)
+        )
+    )
+    ball_state = BallState(
+        Physics(
+            location=Vector3(0, 0, 1500),
+            velocity=Vector3(0, 0, 0),
+            angular_velocity=Vector3(0, 0, 0)
+        )
+    )
+    game_info = GameInfoState(
+        # game_speed=0.8
+    )
+    return GameState(
+        ball=ball_state,
+        cars={index: car_state},
+        game_info=game_info
+    )
+
 
 def aerial_mid_field_frozen_ball(index):
     y = random.randint(1, 4) * -1000
