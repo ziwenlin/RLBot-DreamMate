76b0b290cfa1ec298ac6910b9945f1d5086f5c01
ziwenlin <zw.lin1998@hotmail.com>
2023-04-18 00:32:02 +0200

    Added input queue which collects data

--- src/gui/application.py
+++ src/gui/application.py
@@ -1,6 +1,7 @@
 import random
+import time
 import tkinter as tk
-from multiprocessing import Process, Event
+from multiprocessing import Process, Queue, Event
 from threading import Event as TEvent
 
 from gui.graph import Graph
@@ -10,17 +11,23 @@ class AppProcess(Process):
     def __init__(self):
         super().__init__(name='Tkinter')
         self.stop_event = Event()
+        self.queue_in = Queue()
 
     def run(self) -> None:
-        app = Application(self.stop_event)
+        app = Application(self.stop_event, self.queue_in)
         app.mainloop()
         app.quit()
 
+    def stop(self):
+        self.stop_event.set()
+
 
 class Application(tk.Tk):
-    def __init__(self, event: TEvent):
+    def __init__(self, event: TEvent, queue_in: Queue):
         super().__init__()
         self.event = event
+        self.queue_in = queue_in
+
         self.frame = tk.Frame(self)
         self.frame.pack(fill='both', expand=True)
 
@@ -36,23 +43,36 @@ class Application(tk.Tk):
     def poll_running(self):
         if self.event.is_set():
             self.quit()
+            return
         self.after(100, lambda: self.poll_running())
 
     def poll_data(self):
+        self.after(100, lambda: self.poll_data())
+        if self.queue_in.empty():
+            return
+        value = self.queue_in.get()
         data_y = self.graph.line.get_ydata()
-        data_y += (random.random() * 10 - 1,)
+        data_y += (value,)
         data_x = self.graph.line.get_xdata()
         data_x += (1 + data_x[-1] if len(data_x) > 0 else 0,)
         self.graph.line.set_ydata(data_y)
         self.graph.line.set_xdata(data_x)
         self.graph.plot.set_xlim(-1, len(data_x) + 1)
         self.graph.draw()
-        self.after(100, lambda: self.poll_data())
 
 
 def main():
     process = AppProcess()
     process.start()
+
+    for _ in range(10):
+        time.sleep(0.1)
+        process.queue_in.put(random.random() * 10 - 1)
+
+    time.sleep(2)
+    process.stop()
+
+    time.sleep(1)
     process.join()
 
 
