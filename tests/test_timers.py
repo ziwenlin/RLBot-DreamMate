da05994176625c6bc8bb753f56d0dd04d2637af6
ziwenlin <zw.lin1998@hotmail.com>
2023-08-24 13:49:03 +0200

    Fixed test_timers.py failing because parameter got removed

--- tests/test_timers.py
+++ tests/test_timers.py
@@ -12,42 +12,42 @@ class TestTimedAction(TestCase):
         self.value = True
 
     def test_run1(self):
-        state = self.action.run(None, -1, -2)
+        state = self.action.run(None, -1)
         self.assertFalse(self.value)
         self.assertFalse(state)
 
     def test_run2(self):
-        state = self.action.run(None, -0.5, -1)
+        state = self.action.run(None, -0.5)
         self.assertFalse(self.value)
         self.assertFalse(state)
 
     def test_run3(self):
-        state = self.action.run(None, 0, -0.5)
+        state = self.action.run(None, 0)
         self.assertTrue(self.value)
         self.assertFalse(state)
 
     def test_run4(self):
-        state = self.action.run(None, 0.5, 0)
+        state = self.action.run(None, 0.5)
         self.assertTrue(self.value)
         self.assertFalse(state)
 
     def test_run5(self):
-        state = self.action.run(None, 1, 0.5)
+        state = self.action.run(None, 1)
         self.assertTrue(self.value)
         self.assertFalse(state)
 
     def test_run6(self):
-        state = self.action.run(None, 1.5, 1)
+        state = self.action.run(None, 1.5)
         self.assertTrue(self.value)
         self.assertFalse(state)
 
     def test_zero(self):
         action = TimedAction(0, 0, self.function)
 
-        state = action.run(None, 0, -1)
+        state = action.run(None, 0)
         self.assertTrue(self.value)
         self.assertFalse(state)
 
-        state = action.run(None, 1, 0)
+        state = action.run(None, 1)
         self.assertTrue(self.value)
         self.assertTrue(state)
