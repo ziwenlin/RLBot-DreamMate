2e0f302f47a049534b22c8670e0188d7121deb0a
ziwenlin <zw.lin1998@hotmail.com>
2024-05-18 18:51:03 +0200

    Created class SimpleLogger

--- tools/networking/logger.py
+++ tools/networking/logger.py
@@ -14,3 +14,24 @@ class Logger(Thread):
 
     def logging(self, message):
         self.logger.warning(self.prefix + str(message))
+
+
+class SimpleLogger:
+    def __init__(self, prefix: str):
+        super().__init__()
+        self.prefix = prefix + ' '
+        self.logger = logging.Logger(prefix)
+        self.logger.addHandler(logging.StreamHandler())
+        self.logger.setLevel(logging.INFO)
+
+    def log_debug(self, message: str):
+        self.logger.debug(self.prefix + message)
+
+    def log_info(self, message: str):
+        self.logger.info(self.prefix + message)
+
+    def log_warning(self, message: str):
+        self.logger.warning(self.prefix + message)
+
+    def log_error(self, message: str):
+        self.logger.error(self.prefix + message)
