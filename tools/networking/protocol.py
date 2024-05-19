f0f1d3a495dea42068a9dcf76b9b89871f898461
ziwenlin <zw.lin1998@hotmail.com>
2024-05-19 14:15:05 +0200

    Created class SimpleQueue

--- tools/networking/protocol.py
+++ tools/networking/protocol.py
@@ -6,6 +6,27 @@ import networking.configuration as config
 from networking.logger import SimpleLogger
 
 
+class SimpleQueue:
+    def __init__(self, queue_input=None, queue_output=None):
+        if queue_input is None:
+            queue_input = Queue()
+        if queue_output is None:
+            queue_output = Queue()
+        self.queue_input = queue_input
+        self.queue_output = queue_output
+
+    def connect(self):
+        return SimpleQueue(self.queue_output, self.queue_input)
+
+    def put(self, item):
+        self.queue_output.put(item)
+
+    def get(self):
+        if self.queue_input.empty() is True:
+            return None
+        return self.queue_input.get()
+
+
 class MultiQueue:
     def __init__(self):
         self.queue_input: List[Queue] = []
