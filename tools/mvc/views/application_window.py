5006626cae36e7681841b782efad769f22ae775b
ziwenlin <zw.lin1998@hotmail.com>
2024-06-01 16:58:09 +0200

    Reordered class ApplicationView

--- tools/mvc/views/application_window.py
+++ tools/mvc/views/application_window.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import tkinter.ttk as ttk
-
 from typing import Dict
 
 from mvc.views.client_console import ClientConsoleView
@@ -15,28 +14,33 @@ class ApplicationView:
         self.root.grid_columnconfigure(index=1, weight=1)
         self.root.grid_rowconfigure(index=0, weight=1)
 
-        self.notebook = ttk.Notebook(self.root)
-        self.notebook.grid(GRID_CNF, row=0, column=1)
+        self.frame_consoles = tk.LabelFrame(self.root, text='Console')
+        self.frame_consoles.grid(GRID_CNF, row=0, column=1)
+        self.frame_consoles.grid_rowconfigure(index=0, weight=1)
+        self.frame_consoles.grid_columnconfigure(index=0, weight=1)
 
-        self.frame = tk.LabelFrame(self.root, text='Settings')
-        self.frame.grid(GRID_CNF, row=0, column=0)
+        self.notebook = ttk.Notebook(self.frame_consoles)
+        self.notebook.grid(GRID_CNF)
 
-        self.label_server_address = tk.Label(self.frame, text='Address')
+        self.frame_settings = tk.LabelFrame(self.root, text='Settings')
+        self.frame_settings.grid(GRID_CNF, row=0, column=0)
+
+        self.label_server_address = tk.Label(self.frame_settings, text='Address')
         self.label_server_address.grid(GRID_CNF, row=0, column=0)
 
-        self.entry_server_address = tk.Entry(self.frame)
+        self.entry_server_address = tk.Entry(self.frame_settings)
         self.entry_server_address.grid(GRID_CNF, row=0, column=1, columnspan=2)
 
-        self.label_server_port = tk.Label(self.frame, text='Port')
+        self.label_server_port = tk.Label(self.frame_settings, text='Port')
         self.label_server_port.grid(GRID_CNF, row=1, column=0)
 
-        self.entry_server_port = tk.Entry(self.frame)
+        self.entry_server_port = tk.Entry(self.frame_settings)
         self.entry_server_port.grid(GRID_CNF, row=1, column=1, columnspan=2)
 
-        self.button_spawn_server = tk.Button(self.frame, text='Spawn Server')
+        self.button_spawn_server = tk.Button(self.frame_settings, text='Spawn Server')
         self.button_spawn_server.grid(GRID_CNF, row=3, column=1)
 
-        self.button_spawn_client = tk.Button(self.frame, text='Spawn Client')
+        self.button_spawn_client = tk.Button(self.frame_settings, text='Spawn Client')
         self.button_spawn_client.grid(GRID_CNF, row=3, column=2)
 
         self.clients: Dict[str, ClientConsoleView] = {}
