c95c72dfb85a3637c85d950cf013ad6d073fe2f7
ziwenlin <zw.lin1998@hotmail.com>
2024-06-01 16:56:03 +0200

    Created notebook_console.py from client_console.py and server_console.py

---
+++ tools/mvc/views/notebook_console.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+
+from mvc.views.view_style import PADDING_CNF, GRID_CNF
+
+
+class NotebookConsoleView:
+    def __init__(self, master):
+        self.master = master
+        self.frame = tk.Frame(master)
+        self.frame.grid_rowconfigure(index=0, weight=0)
+        self.frame.grid_rowconfigure(index=1, weight=1)
+        self.frame.grid_columnconfigure(index=0, weight=1)
+
+        self.frame_control = tk.LabelFrame(self.frame, PADDING_CNF, text='Controls')
+        self.frame_control.grid(GRID_CNF)
+        for index in range(9):
+            self.frame_control.grid_columnconfigure(index=index, weight=1)
+        self.frame_control.grid_rowconfigure(index=0, weight=0)
+
+        self.entry_message = tk.Entry(self.frame_control)
+        self.entry_message.grid(GRID_CNF, row=0, column=0, columnspan=3)
+
+        self.button_send_message = tk.Button(self.frame_control, text='Send message')
+        self.button_send_message.grid(GRID_CNF, row=0, column=3)
+
+        self.button_start_server = tk.Button(self.frame_control, text='Start connection')
+        self.button_start_server.grid(GRID_CNF, row=0, column=4)
+
+        self.button_stop_server = tk.Button(self.frame_control, text='Stop connection')
+        self.button_stop_server.grid(GRID_CNF, row=0, column=5)
+
+        self.button_close = tk.Button(self.frame_control, text='Close')
+        self.button_close.grid(GRID_CNF, row=0, column=8)
+
+        self.frame_chat = tk.LabelFrame(self.frame, PADDING_CNF, text='Chat')
+        self.frame_chat.grid(GRID_CNF)
+        self.frame_chat.grid_rowconfigure(index=0, weight=1)
+        self.frame_chat.grid_columnconfigure(index=0, weight=1)
+
+        self.textbox_chat = tk.Text(self.frame_chat)
+        self.textbox_chat.grid(GRID_CNF)
