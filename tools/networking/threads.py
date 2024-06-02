5002aab8560ab312786ccd2b10d75ea2a5a117f1
ziwenlin <zw.lin1998@hotmail.com>
2024-06-02 23:35:35 +0200

    Added field SimpleQueue

--- tools/networking/threads.py
+++ tools/networking/threads.py
@@ -1,10 +1,13 @@
 import threading
 
+from networking.queues import SimpleQueue
+
 
 class SimpleThread(threading.Thread):
     def __init__(self):
         super().__init__()
         self.running = threading.Event()
+        self.queues = SimpleQueue()
 
     def start(self) -> None:
         self.running.set()
